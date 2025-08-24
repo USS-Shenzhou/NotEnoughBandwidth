28a4511995b4fe483cf16aad23f55e2b7dff22d4
USS-Shenzhou <969840795@qq.com>
2025-08-24 11:13:19 +0800

    fabric entry class

---
+++ 1.18.2-fabric/src/main/java/cn/ussshenzhou/client/ExampleC.java
@@ -0,0 +1,10 @@
+package cn.ussshenzhou.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleC implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
